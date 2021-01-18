693116ab98679d2423225fc9e24e4ef0a7fe043b
t440 <keenanbachchrist@gmail.com>
2021-01-18 18:42:01 +0100

    added sensor class

---
+++ temp_measurer.py
@@ -0,0 +1,14 @@
+#!./venv/bin/python3
+
+#import Adafruit_DHT
+#import time
+
+
+#class TempMeasurer:
+#    def __init__(self):
+#        self.DHT_sensor = Adafruit_DHT.DHT22
+#        self.DHT_pin = x
+
+#    def measure(self):
+#        humidity, temperature = Adafruit_DHT.read(self.DHT_sensor, self.DHT_pin)
+#        return newVal[temperature, humidity]
